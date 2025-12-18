0ac93c01a659bc1eceb7b2d94bb9869faf2c39e8
Irfan Saf <irfansaf7@gmail.com>
2025-12-18 18:04:12 +0700

    refactor: lint

--- internal/modules/patient/dto/patient.go
+++ internal/modules/patient/dto/patient.go
@@ -7,23 +7,23 @@ import (
 )
 
 type CreatePatientRequest struct {
-	FirstName   string  `json:"first_name" validate:"required"`
-	LastName    string  `json:"last_name" validate:"required"`
+	FirstName   string  `json:"first_name"    validate:"required"`
+	LastName    string  `json:"last_name"     validate:"required"`
 	DateOfBirth string  `json:"date_of_birth" validate:"required,datetime=2006-01-02"`
-	Email       *string `json:"email" validate:"omitempty,email"`
+	Email       *string `json:"email"         validate:"omitempty,email"`
 	Phone       *string `json:"phone"`
 	Address     *string `json:"address"`
-	Status      string  `json:"status" validate:"omitempty,oneof=active inactive archived"`
+	Status      string  `json:"status"        validate:"omitempty,oneof=active inactive archived"`
 }
 
 type UpdatePatientRequest struct {
 	FirstName   string  `json:"first_name"`
 	LastName    string  `json:"last_name"`
 	DateOfBirth string  `json:"date_of_birth" validate:"omitempty,datetime=2006-01-02"`
-	Email       *string `json:"email" validate:"omitempty,email"`
+	Email       *string `json:"email"         validate:"omitempty,email"`
 	Phone       *string `json:"phone"`
 	Address     *string `json:"address"`
-	Status      string  `json:"status" validate:"omitempty,oneof=active inactive archived"`
+	Status      string  `json:"status"        validate:"omitempty,oneof=active inactive archived"`
 }
 
 type PatientResponse struct {
