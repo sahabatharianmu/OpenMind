4675a01bfa48ecd1e7ab801e61bae48750fc28fc
Irfan Saf <irfansaf7@gmail.com>
2025-12-19 08:18:13 +0700

    [OMDEV-2] Add: GORM hooks for signed note immutability (BeforeUpdate and BeforeDelete)

--- internal/modules/clinical_note/entity/clinical_note.go
+++ internal/modules/clinical_note/entity/clinical_note.go
@@ -1,6 +1,7 @@
 package entity
 
 import (
+	"errors"
 	"time"
 
 	"github.com/google/uuid"
@@ -62,4 +63,42 @@ func (Attachment) TableName() string {
 
 func (ClinicalNote) TableName() string {
 	return "clinical_notes"
+}
+
+// BeforeUpdate is a GORM hook that prevents updates to signed notes
+// This provides application-level protection in addition to database triggers
+func (n *ClinicalNote) BeforeUpdate(tx *gorm.DB) error {
+	// Check if the note was already signed before this update
+	// We need to query the database to get the current state
+	var existingNote ClinicalNote
+	if err := tx.Unscoped().Where("id = ?", n.ID).First(&existingNote).Error; err != nil {
+		// If note doesn't exist, allow the operation (it will fail elsewhere)
+		return nil
+	}
+
+	// If the note was already signed, prevent the update
+	if existingNote.IsSigned {
+		return errors.New("cannot update a signed clinical note: signed notes are immutable for compliance")
+	}
+
+	return nil
+}
+
+// BeforeDelete is a GORM hook that prevents deletion of signed notes
+// This provides application-level protection in addition to database triggers
+func (n *ClinicalNote) BeforeDelete(tx *gorm.DB) error {
+	// Check if the note is signed
+	// We need to query the database to get the current state
+	var existingNote ClinicalNote
+	if err := tx.Unscoped().Where("id = ?", n.ID).First(&existingNote).Error; err != nil {
+		// If note doesn't exist, allow the operation (it will fail elsewhere)
+		return nil
+	}
+
+	// If the note is signed, prevent deletion
+	if existingNote.IsSigned {
+		return errors.New("cannot delete a signed clinical note: signed notes are immutable for compliance")
+	}
+
+	return nil
 }
